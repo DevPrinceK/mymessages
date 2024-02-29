7c395ef907c066725546b78dc4a651295576e9f7
princek <princesamuelpks@gmail>
2024-02-29 06:15:50 +0000

    Create data_model.dart

---
+++ lib/models/data_model.dart
@@ -0,0 +1,37 @@
+class VideoModel {
+  String title;
+  String preacher;
+  String videoUrl;
+  String thumbnail;
+
+  VideoModel({
+    required this.title,
+    required this.thumbnail,
+    required this.videoUrl,
+    required this.preacher,
+  });
+}
+
+class AudioModel {
+  String title;
+  String preacher;
+  String audioUrl;
+
+  AudioModel({
+    required this.title,
+    required this.audioUrl,
+    required this.preacher,
+  });
+}
+
+class TextModel {
+  String title;
+  String message;
+  String preacher;
+
+  TextModel({
+    required this.title,
+    required this.message,
+    required this.preacher,
+  });
+}
